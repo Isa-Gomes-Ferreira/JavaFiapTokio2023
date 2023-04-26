1ffcbc3730040433cde8fedf967af9db45524f07
logonrmlocal <logonrmlocal@PA402MICRO12.fiap.com.br>
2023-04-26 18:28:48 -0300

    Aulas 08 e 09.

---
+++ JavaTokio2023/src/logica/aulas/aula08/Datas.java
@@ -0,0 +1,32 @@
+package logica.aulas.aula08;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Datas {
+
+	public static void main(String[] args) {
+		
+		//instanciando 
+		Date data = new Date();
+		System.out.println(data);
+		
+		// instanciando uma classe abstrata
+		Calendar c = Calendar.getInstance();
+		
+		System.out.println("Data e hora atual: " + c.getTime());
+		System.out.println("Ano: " + c.get(Calendar.YEAR));
+		System.out.println("Mês: " + c.get(Calendar.MONTH));
+		System.out.println("Dia do mês: " + c.get(Calendar.DAY_OF_MONTH));
+		System.out.println("Dia da semana: " + c.get(Calendar.DAY_OF_WEEK));
+		
+		int hora = c.get(Calendar.HOUR_OF_DAY);
+		int min = c.get(Calendar.MINUTE);
+		
+		System.out.println("");
+		System.out.println("Hora: " + hora);
+		System.out.println("Minutos: " + min);
+		
+	}
+
+}
---
+++ JavaTokio2023/src/logica/aulas/aula09/Vetores.java
@@ -0,0 +1,45 @@
+package logica.aulas.aula09;
+
+public class Vetores {
+
+	public static void main(String[] args) {
+		
+		/*
+		String[] listaFrutas = new String[4];
+		
+		listaFrutas[0] = "Kiwi";
+		listaFrutas[1] = "Jabuticaba";
+		listaFrutas[2] = "Maracuja doce";
+		listaFrutas[3] = "Pitaia";
+		
+		//System.out.println("Fruta pos 1: " + listaFrutas[1]);
+		
+		String[] listaFrutas2 = {"Manga", "Jaca", "Tangerina", "Pitomba"};
+		
+		int qntFrutas = listaFrutas2.length;
+		//System.out.println(qntFrutas);
+		
+		for (int i = 0; i < qntFrutas; i++) {
+			System.out.println(listaFrutas2[i]);
+		}
+		*/
+		
+		
+		// Vetores numéricos //
+		
+		int[] numeros = {0, 5, 20, 30};
+		
+		// FOR INDEXADO
+		
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.printf("Pos: %d -- Valor: %d \n", i, numeros[i]);
+		}
+		
+		/*
+		int qntNotas = 10;
+		double[] notas = new double[5];
+		 */
+		
+	}
+
+}
