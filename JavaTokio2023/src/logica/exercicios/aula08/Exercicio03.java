513788306068a9d9623d0f9f2fa7f51a682b4b85
logonrmlocal <logonrmlocal@PA402MICRO12.fiap.com.br>
2023-05-08 17:37:54 -0300

    Fazendo exxercícios

--- JavaTokio2023/src/logica/exercicios/aula08/Exercicio03.java
+++ JavaTokio2023/src/logica/exercicios/aula08/Exercicio03.java
@@ -11,12 +11,35 @@ public class Exercicio03 {
 		System.out.println("Digite o seu texto: ");
 		String texto = sc.nextLine();
 		
+		int mai = 0;
+		int min = 0;
+		int num = 0;
+		int esp = 0;
+		
 		for (int i = 0; i < texto.length(); i++) {
 			
-			//if () {
-				
-			//}
+			if ((texto.charAt(i) >= 'A') && (texto.charAt(i) <= 'Z')) {
+				mai = mai + 1; 
+			}
+			
+			if ((texto.charAt(i) >= 'a') && (texto.charAt(i) <= 'z')) {
+				min = min + 1; 
+			}
+			
+			if ((texto.charAt(i) >= '0') && (texto.charAt(i) <= '9')) {
+				num = num + 1; 
+			}
+			
+			if ((texto.charAt(i) >= ' ') && (texto.charAt(i) <= ' ')) {
+				esp = esp + 1; 
+			}
+			
 		}
+		
+		System.out.println(mai);
+		System.out.println(min);
+		System.out.println(num);
+		System.out.println(esp);
 		
 		sc.close();
 	}
